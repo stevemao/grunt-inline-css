68270dbb680f27783af70304b17789f895eaac72
steve mao <steve.mao@healthinteract.com.au>
2014-07-01 21:56:36 +1000

    style the source, change inlineCss to a named function

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,58 +8,58 @@
 
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>',
-      ],
-      options: {
-        jshintrc: '.jshintrc',
-      },
-    },
-
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp'],
-    },
-
-    // Configuration to be run (and then tested).
-    inlinecss: {
-      basic: {
-        options: {
-          extraCss: 'body { background: green; }'
+    // Project configuration.
+    grunt.initConfig({
+        jshint: {
+            all: [
+                'Gruntfile.js',
+                'tasks/*.js',
+                '<%= nodeunit.tests %>',
+            ],
+            options: {
+                jshintrc: '.jshintrc',
+            },
         },
-        files: {
-          'tmp/out.html': 'test/fixtures/in.html'
+
+        // Before generating any new files, remove any previously-created files.
+        clean: {
+            tests: ['tmp'],
         },
-      },
-    },
 
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js'],
-    },
+        // Configuration to be run (and then tested).
+        inlineCss: {
+            basic: {
+                options: {
+                    extraCss: 'body { background: green; }'
+                },
+                files: {
+                    'tmp/out.html': 'test/fixtures/in.html'
+                },
+            },
+        },
 
-  });
+        // Unit tests.
+        nodeunit: {
+            tests: ['test/*_test.js'],
+        },
 
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
+    });
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+    // Actually load this plugin's task(s).
+    grunt.loadTasks('tasks');
 
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'inlinecss', 'nodeunit']);
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
+    // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+    // plugin's task(s), then test the result.
+    grunt.registerTask('test', ['clean', 'inlineCss', 'nodeunit']);
+
+    // By default, lint and run all tests.
+    grunt.registerTask('default', ['jshint', 'test']);
 
 };
--- tasks/inline_css.js
+++ tasks/inline_css.js
@@ -8,14 +8,14 @@
 
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
 
   var juice = require('juice');
 
-  grunt.registerMultiTask('inlinecss', 'Takes an html file with css link and turns inline.  Great for emails.', function() {
+  grunt.registerMultiTask('inlineCss', 'Takes an html file with css link and turns inline.  Great for emails.', function () {
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options();
     var done = this.async();
@@ -23,9 +23,12 @@ module.exports = function(grunt) {
     var count = this.files.length;
 
     // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
+    this.files.forEach(function (file) {
+      inlinCss(file);
+    });
 
-      var filepath = f.src.toString();
+    function inlinCss(file) {
+      var filepath = file.src.toString();
       if (typeof filepath !== 'string' || filepath === '') {
         grunt.log.error('src must be a single string');
         return false;
@@ -36,14 +39,14 @@ module.exports = function(grunt) {
         return false;
       }
 
-      juice(filepath, options, function(err, html) {
+      juice(filepath, options, function (err, html) {
 
         if (err) {
           return grunt.log.error(err);
         }
 
-        grunt.file.write(f.dest, html);
-        grunt.log.writeln('File "' + f.dest + '" created.');
+        grunt.file.write(file.dest, html);
+        grunt.log.writeln('File "' + file.dest + '" created.');
 
 
         index++;
@@ -52,8 +55,8 @@ module.exports = function(grunt) {
         }
 
       });
+    }
 
-    });
   });
 
 };
